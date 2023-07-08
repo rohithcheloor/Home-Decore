47aa788e8bccdbf9efb85315808f1979634c6e75
Rohith Cheloor <rohithcm101@gmail.com>
2023-07-08 11:01:50 -0400

    Initial commit

---
+++ app/src/main/java/com/rohithcheloor/homedecore/DetailActivity.kt
@@ -0,0 +1,11 @@
+package com.rohithcheloor.homedecore
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class DetailActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
